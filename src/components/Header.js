321a9910338f8a86af123bcf5eff650cf4383c94
BobWRowsse <bobrowsse.walakira@hyperisland.se>
2021-05-20 18:20:26 +0200

    BURGER MENU Animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,10 +19,6 @@ function Header() {
                 {cars && cars.map((car, index) => (
                     <a key={index} href="#">{car}</a>
                 ))}
-                <a href="#">Model S</a>
-                <a href="#">Model Y</a>
-                <a href="#">Model 3</a>
-                <a href="#">Model X</a>
             </Menu>
             <RightMenu>
                 <a href="#">Shop</a>
